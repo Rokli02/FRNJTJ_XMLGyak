a368469fd00d25b6729089d419cde4586f28c5e3
Roló <71132065+Rokli02@users.noreply.github.com>
2021-10-19 15:21:29 +0200

    Add files via upload

---
+++ FRNJTJ_1019/Eclipse/SaxFRNJTJ/src/SaxFRNJTJ1019/SaxFRNJTJ.java
@@ -0,0 +1,28 @@
+package SaxFRNJTJ1019;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+
+public class SaxFRNJTJ {
+
+	public static void main(String[] args) {
+		try {
+			
+			SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
+			SAXParser saxParser = saxParserFactory.newSAXParser();
+			SaxHandler handler = new SaxHandler();
+			saxParser.parse(new File("src/SaxFRNJTJ1019/szemelyekFRNJTJ.xml"), handler);
+			
+		} catch(ParserConfigurationException | SAXException | IOException pce) {
+			System.out.println("Hibaüzenet: "+pce.getMessage());
+		}
+	}
+	
+}
---
+++ FRNJTJ_1019/Eclipse/SaxFRNJTJ/src/SaxFRNJTJ1019/SaxHandler.java
@@ -0,0 +1,56 @@
+package SaxFRNJTJ1019;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.helpers.DefaultHandler;
+
+//tartalomkezelõ keret létrehozása, valamint az esemény- és hibakezelõ metódusok definiálása
+public class SaxHandler extends DefaultHandler {
+
+	private int indent = 0;
+	
+	private String formatAttributes(Attributes attributes) {
+		int attrLength = attributes.getLength();
+		if(attrLength == 0)
+			return "";
+		StringBuilder sb = new StringBuilder(",{");
+		for (int i = 0; i < attrLength; i++) {
+			sb.append(attributes.getLocalName(i)+":"+attributes.getValue(i));
+			if(i < attrLength - 1) {
+				sb.append(", ");
+			}
+		}
+		sb.append("}");
+		return sb.toString();
+	}
+	
+	private void indent() {
+		for (int i = 0; i < indent; i++) {
+			System.out.print(" ");
+		}
+	}
+	
+
+	public void startElement(String uri, String localName, String qName, Attributes attributes) {
+		indent++;
+		indent();
+		System.out.println(qName + formatAttributes(attributes) + " start");
+	}
+	
+	public void endElement(String uri, String localName, String qName, Attributes attributes) {
+		indent();
+		indent--;
+		System.out.println(qName + " end");
+	}
+	
+	public void characters(char ch[], int start, int length) {
+		String chars = new String(ch, start, length);
+		if(!chars.isEmpty()) {
+			indent++;
+			indent();
+			indent--;
+			indent();
+			System.out.println();
+		}
+	}
+	
+}
