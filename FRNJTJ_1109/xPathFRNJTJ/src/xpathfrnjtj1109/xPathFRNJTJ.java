5eb73b56ad615dbfb45413500e1b0e53a3b58489
Roló <71132065+Rokli02@users.noreply.github.com>
2021-11-09 16:05:19 +0100

    Add files via upload

--- FRNJTJ_1109/xPathFRNJTJ/src/xpathfrnjtj1109/xPathFRNJTJ.java
+++ FRNJTJ_1109/xPathFRNJTJ/src/xpathfrnjtj1109/xPathFRNJTJ.java
@@ -23,7 +23,7 @@ public class xPathFRNJTJ {
 			
 			XPath xPath = XPathFactory.newInstance().newXPath();
 			
-			String expression = getExpression(8);
+			String expression = getExpression(12);
 			
 			
 			NodeList nodeList = (NodeList) xPath.compile(expression).evaluate(doc, XPathConstants.NODESET);
@@ -52,7 +52,7 @@ public class xPathFRNJTJ {
 	
 	private static String getExpression(int number) {
 		switch (number) {
-		case 0: return "class";
+		case 0: return "class";					//	Kiindulópont
 		case 1: return "class/student";				//	1) Válassza ki az összes student element, amely a class gyermekei.
 		case 2: return "class/student[@id='01']";	//	2) Válassza ki azt a student elemet, amely rendelkezik "id" attribútummal és értéke "01".
 		case 3: return "//student";					//	3) Kiválasztja az összes student elemet, függetlenül attól, hogy hol vannak a dokumentumban.
@@ -60,11 +60,11 @@ public class xPathFRNJTJ {
 		case 5: return "class/student[last()]";		//	5) Válassza ki a utolsó student elemet, amely a class elem gyermeke.
 		case 6: return "class/student[last()-1]";	//	6) Válassza ki a utolsó elõtti student elemet, amely a class elem gyermeke.
 		case 7: return "class/student[position()<3]";//	7) Válassza ki az elsõ két student elemet, amelyek a class elem gyermekei.
-		case 8: return "class/*";	//	8) Válassza ki class elem összes gyermek elemét
-		case 9: return "";	//	9) Válassza ki az összes student elemet, amely rendelkezik legalább egy bármilyen attribútummal.
-		case 10: return "";	//	10) Válassza ki a dokumentum összes elemét.
-		case 11: return "";	//	11) Válassza ki a class elem összes student elemét, amelynél a kor>20.
-		case 12: return "";	//	12) Válassza ki az összes student elem összes keresztnev és vezeteknev csommópontot.
+		case 8: return "class/*";					//	8) Válassza ki class elem összes gyermek elemét
+		case 9: return "//student[@*]";				//	9) Válassza ki az összes student elemet, amely rendelkezik legalább egy bármilyen attribútummal.
+		case 10: return "*";						//	10) Válassza ki a dokumentum összes elemét.
+		case 11: return "class/student[kor>20]";	//	11) Válassza ki a class elem összes student elemét, amelynél a kor>20.
+		case 12: return "//student/keresztnev | //student/vezeteknev";	//	12) Válassza ki az összes student elem összes keresztnev és vezeteknev csommópontot.
 
 		default: return "";
 		}
